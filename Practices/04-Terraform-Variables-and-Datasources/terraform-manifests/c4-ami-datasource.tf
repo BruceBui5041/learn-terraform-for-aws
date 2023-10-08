18030dc5c4675144d375d22c887aa3358c3fc00a
Bui Hong Lan <lan@hydrax.io>
2023-10-08 23:59:16 +0700

    04-learn datasource

--- Practices/04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
+++ Practices/04-Terraform-Variables-and-Datasources/terraform-manifests/c4-ami-datasource.tf
@@ -1,1 +1,25 @@
+# Latest AMI ID for Amazon Linux2 OS
+data "aws_ami" "amzlinux2" {
+  most_recent = true # NOTE: (Optional) If more than one result is returned, use the most recent AMI
+  owners      = ["amazon"]
 
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
