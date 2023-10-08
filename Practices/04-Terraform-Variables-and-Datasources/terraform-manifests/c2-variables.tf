cc23f89ea7dc9f318378de4b63ef2d6e7be879ea
Bui Hong Lan <lan@hydrax.io>
2023-10-08 23:40:07 +0700

    04-create security group

---
+++ Practices/04-Terraform-Variables-and-Datasources/terraform-manifests/c2-variables.tf
@@ -0,0 +1,20 @@
+# AWS region variable
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+# AWS EC2 Instance Type
+variable "instance_type" {
+  description = "EC2 Instance Type"
+  type        = string
+  default     = "t2.micro"
+}
+
+# AWS EC2 Key Pair
+variable "instance_keypair" {
+  description = "EC2 keypair"
+  type        = string
+  default     = "terraform-key"
+}
